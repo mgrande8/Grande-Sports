034e2fdca3068d1e28f9ef0ca0833cf933f55911
mgrande8 <miguelangelgrande8@gmail.com>
2026-01-15 19:52:05 -0500

    Fix logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,9 +47,9 @@ export default function Header({ showNav = true }: HeaderProps) {
             <Image
               src="/logo-white.png"
               alt="Grande Sports"
-              width={180}
-              height={50}
-              className="h-10 w-auto"
+              width={150}
+              height={40}
+              className="h-8 w-auto object contain"
               priority
             />
           </Link>
